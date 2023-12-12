72e0e5948ee7602a61f819728b8794311deac8c3
SkripkaRS <romanskripka80@gmail.com>
2023-12-12 17:00:08 +0200

    init-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" id="exampleInput" placeholder="Input component" />
+    </div>
+  );
+};
+
+export default Input;
